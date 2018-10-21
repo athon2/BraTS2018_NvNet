4fdc843f4058c3dc4087269fcb08bf2cf47a2d3f
zhoukai <530438548@qq.com>
2018-10-21 02:55:04 +0000

    init dataset

---
+++ dataset.py
@@ -0,0 +1,13 @@
+import os 
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+
+class BratsDataset(Dataset):
+    def __init__(self):
+        pass
+    
+    def __getitem__(self, index):
+        pass
+    
+    def __len__(self):
+        pass
